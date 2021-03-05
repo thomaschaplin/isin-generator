44757da38153133c2387ec2be3acc1b72c02d99a
Thomas Chaplin <thomas.chaplin@tray.io>
2021-03-05 22:44:45 +0000

    refactor

--- index.js
+++ index.js
@@ -4,42 +4,60 @@ const countryCodes = require('./country-codes.json')
 const cusip = require('@thomaschaplin/cusip-generator').generateCusip()
 
 
-const generateIsin = cusip => {
-    let code = countryCodes[Math.floor(Math.random() * countryCodes.length)]
-    let isin = code + cusip + generateIsinCheckDigit(code + cusip + '0')
+const generateIsin = (countryCode) => {
+    const code = countryCode ? countryCode : countryCodes[Math.floor(Math.random() * countryCodes.length)]
+    const isin = code + cusip + generateIsinCheckDigit(code + cusip + '0')
     return isin
 }
 
-const generateIsinCheckDigit = isin => {
-    if (isin.length != 12) return false;
-    var v = [];
-    for (var i = isin.length - 2; i >= 0; i--) {
-        let c = isin.charAt(i);
-        if (isNaN(c)) {
-            var letterCode = isin.charCodeAt(i) - 55;
-            v.push(letterCode % 10);
+const isIsinCorrectLength = (isin) => {
+    if(isin.length !== 12) {
+        throw new Error("Invalid cusip")
+    }
+}
+
+const calculateValues = (isin) => {
+    const values = [];
+
+    for (let i = 10; i >= 0; i--) {
+        const char = isin.charAt(i);
+        if (isNaN(char)) {
+            const letterCode = isin.charCodeAt(i) - 55;
+            values.push(letterCode % 10);
             if (letterCode > 9) {
-                v.push(Math.floor(letterCode / 10));
+                values.push(Math.floor(letterCode / 10));
             }
         } else {
-            v.push(Number(c));
+            values.push(Number(char));
         }
     }
-    var sum = 0;
-    var l = v.length;
-    for (var i = 0; i < l; i++) {
+    return values
+}
+
+const calculateCheckDigit = (values) => {
+    let sum = 0;
+    for (let i = 0; i < values.length; i++) {
         if (i % 2 == 0) {
-            var d = v[i] * 2;
+            const d = values[i] * 2;
             sum += Math.floor(d / 10)
             sum += d % 10;
         } else {
-            sum += v[i];
+            sum += values[i];
         }
     }
     return (10 - (sum % 10)) % 10
+}
+
+const generateIsinCheckDigit = isin => {
+    isIsinCorrectLength(isin)
+    const values = calculateValues(isin)
+    const checkDigit = calculateCheckDigit(values)
+    return checkDigit
 }
 
 
 exports.isinGenerator = function isinGenerator() {
     return generateIsin(cusip)
 }
+
+// console.log(generateIsin())
